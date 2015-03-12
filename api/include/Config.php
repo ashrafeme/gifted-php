8a01ff52893c760dc782040be9797b0ece2ea16c
ashrafeme <ashraf.main@gmail.com>
2015-03-12 20:01:03 +0000

    remove config data

--- api/include/Config.php
+++ api/include/Config.php
@@ -9,15 +9,10 @@ define('DB_USERNAME', 'ashrafeme');
 define('DB_PASSWORD', '');
 define('DB_HOST', 'ashrafeme-gifted-1315229');
 define('DB_NAME', 'giftedsa');
-define('DB_Port', '3306');
+define('DB_Port', '3306');//Project Number: 767709955395
 
- /*
- 
-define('DB_USERNAME', 'giftededu');
-define('DB_PASSWORD', 'javajava');
-define('DB_HOST', 'db4free.net');
-define('DB_NAME', 'giftedsa');
-*/
+
+
 
 define('USER_CREATED_SUCCESSFULLY', 0);
 define('USER_CREATE_FAILED', 1);
